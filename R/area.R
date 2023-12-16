7c5f551741c36a05839ba632902b7fd6c4c2b92c
Eoghan-Clancy <eoghan.clancy.2021@mumail.ie>
2023-12-16 10:22:34 +0000

    Area function/methods for 2D and 3D shapes

---
+++ R/area.R
@@ -0,0 +1,36 @@
+area <- function(obj) {
+  if(!inherits(obj,"shape")){
+    stop("area can only be passed shape objs")
+  }
+  else{
+    UseMethod("area")
+  }
+}
+
+#2D
+area.rectangle <- function(obj) {
+  return(obj$length * obj$width)
+}
+
+area.circle <- function(obj) {
+  return(pi*obj$radius^2)
+}
+
+area.triangle <- function(obj) {
+  s <- perimeter(obj)/2
+  Area <- sqrt(s*(s-obj$a)*(s-obj$b)*(s-obj$c)) #Heron's formula
+  return(Area)
+}
+
+#3D - these are surface areas
+area.cuboid <- function(obj){
+  return(2 * (obj$length * obj$width + obj$length * obj$height + obj$height * obj$width))
+}
+
+area.sphere <- function(obj){
+  return(4*pi*obj$radius^2)
+}
+
+area.cylinder <- function(obj){
+  return(2*pi*obj$radius*(obj$radius+obj$height))
+}
