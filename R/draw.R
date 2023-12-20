74644df612e8b0e2ab18c89c6387a0df235b0263
Eoghan-Clancy <eoghan.clancy.2021@mumail.ie>
2023-12-20 18:39:31 +0000

    Remade the 2d draw functions to use ggplot and added border color and fill color arguments to said methods. Also modified draw and the 3d draw methods to be able to take in the additional 2 parameters

--- R/draw.R
+++ R/draw.R
@@ -1,4 +1,4 @@
-draw <- function(shape) {
+draw <- function(shape, color = "blue", fillColor = "transparent") {
   if(!inherits(shape,"shape")){
     stop("Draw can only be passed objects of class shape")
   }
@@ -7,38 +7,68 @@ draw <- function(shape) {
   }
 }
 
-draw.rectangle <- function(rect) {
-  x <- c(0, 0, rect$width, rect$width, 0)
-  y <- c(0, rect$height, rect$height, 0, 0)
+#2D shapes
+draw.rectangle <- function(rec, color = "blue", fillColor = "transparent") {
+  rectangle_data <- data.frame(
+    x = c(0, rec$width, rec$width, 0),
+    y = c(0, 0, rec$height, rec$height)
+  )
 
-  plot(x, y, type = "l", asp = 1)
+  max_dim <- max(rec$height, rec$width)
+
+  p <- ggplot(rectangle_data, aes(x, y)) +
+    geom_polygon(fill = fillColor, color = color, size = 2) +
+    labs(x = "Width", y = "Height", title = "Rectangle") +
+    theme_minimal() +
+    coord_fixed(ratio = 1) +
+    lims(x = c(0, max_dim), y = c(0, max_dim))
+
+  print(p)
 }
 
-draw.triangle <- function(tri) {
-  x <- c(0, tri$b, tri$c, 0)
-  y <- c(0, 0, tri$a, 0)
+draw.triangle <- function(triangle, color = "blue", fillColor = "transparent") {
+  triangle_data <- data.frame(
+    x = c(0, triangle$a, triangle$b),
+    y = c(0, 0, triangle$c)
+  )
 
-  plot(x, y, type = "l", asp = 1)
+  max_dim <- max(triangle$a, triangle$b, triangle$c)
+
+  p <- ggplot(triangle_data, aes(x, y)) +
+    geom_polygon(fill = fillColor, color = color, size = 2) +
+    labs(x = "Base", y = "Height", title = "Triangle") +
+    theme_minimal() +
+    coord_fixed(ratio = 1) +
+    lims(x = c(0, max_dim), y = c(0, max_dim))
+
+  print(p)
 }
 
-draw.circle <- function(circ){
+draw.circle <- function(circle, color = "blue", fillColor = "transparent") {
+  circle_data <- data.frame(
+    x = circle$radius * cos(seq(0, 2 * pi, length.out = 100)),
+    y = circle$radius * sin(seq(0, 2 * pi, length.out = 100))
+  )
 
-  theta <- seq(0, 2 * pi, length.out = 100)
+  p <- ggplot(circle_data, aes(x, y)) +
+    geom_polygon(fill = fillColor, color = color, size = 2) +
+    labs(x = "X-axis", y = "Y-axis", title = "Circle") +
+    theme_minimal() +
+    coord_fixed(ratio = 1)
 
-  x <- circ$radius * cos(theta)
-  y <- circ$radius * sin(theta)
-
-  plot(x,y, type = "l", asp = 1)
+  print(p)
 }
 
-draw.sphere <- function(sph, color =  "#FF0000") {
+
+# 3D shapes
+draw.sphere <- function(sph, color =  "#FF0000",..) {
   open3d()
 
   spheres3d(0, 0, 0, radius = sph$radius, color = color, alpha = 0.7)
   decorate3d(box = FALSE ,axes = TRUE)
 }
 
-draw.cuboid <- function(cub, color =  "red") {
+draw.cuboid <- function(cub, color =  "red", ...) {
   height <- cub$height
   width <- cub$width
   depth <- cub$depth
@@ -74,11 +104,11 @@ draw.cuboid <- function(cub, color =  "red") {
   decorate3d()
 }
 
-draw.cylinder <- function(cyl){
+draw.cylinder <- function(cyl, color = "blue", ..){
   center <- matrix(c(0, 0, 0, 0, 0, cyl$height), ncol = 3, byrow = TRUE)
   radius <- cyl$radius
   cylinder_mesh <- cylinder3d(center = center, radius = radius, sides = 1000,closed=-2)
   open3d()
-  shade3d(cylinder_mesh, color = "blue", alpha = 0.7)
+  shade3d(cylinder_mesh, color = color, alpha = 0.7)
   decorate3d()
 }
